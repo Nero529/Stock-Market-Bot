7492fce87e7272552ddec8b9588c673b3dd0968f
Quamez Anderson <quamez19@aol.com>
2021-03-04 16:42:35 -0600

    Update auto_trader.py

--- auto_trader.py
+++ auto_trader.py
@@ -10,6 +10,8 @@ import qrcode
 from PIL import Image
 import os
 import pandas as pd
+import numpy as np
+import math
 
 def check_market_open():
     '''
@@ -57,63 +59,110 @@ def is_time_interval(minute = -1, hour = -1, second = -1):
         time.sleep(1)
         return False
 
-def get_ema(current_price, period, past_ema=0, past_close_prices):
+def get_ema(current_price, period, past_close_prices):
     
-    if past_ema != 0:
+    if not pd.isnull(all_share_data.iloc[share_index[index_num]]['EMA{}'.format(period)]):
+        past_ema = all_share_data.iloc[share_index[index_num]]['EMA{}'.format(period)]
         new_ema = current_price * (2 / (period + 1)) + past_ema * (1- (2 / (period + 1)))
     else:
-        for index, mov_avg in enumerate(past_close_prices[:period]):
-            if index == 0:
-                mov_avg = 0
-            mov_avg += x
+        mov_avg = 0
+        for value in past_close_prices[:period]:
+            mov_avg += float(value)
         new_ema = mov_avg / period
 
+    all_share_data.loc[share_index[index_num], 'EMA{}'.format(period)] = new_ema
     return new_ema
 
-def get_macd(period_one, period_two):
+def get_macd(ema_one, ema_two, period, sma_list=[]):
+    
+    macd = ema_one - ema_two
+    if not pd.isnull(all_share_data.iloc[share_index[index_num]]['MACD']):
+        prev_macd = all_share_data.iloc[share_index[index_num]]['MACD']
+        macd_signal = macd * (2 / (period + 1)) + prev_macd * (1 - (2 / (period + 1)))
+        macd_histo = macd - macd_signal
+        #print('MACD Histo:', macd_histo)
+        if macd_histo > 0:
+            signal = 'BUY'
+        elif macd_histo < 0:
+            signal = 'SELL'
+            
+        return signal
+    else:
+        sma_list.append(macd)
+        sma = 0
+        for value in sma_list:
+            sma += value
+        sma /= period
+        if len(sma_list) == period:
+            all_share_data.loc[share_index[index_num], 'MACD'] = sma
+    
 
 
 
 
-
-def get_stochastic(current_value, period, past_stochastic):
+def get_stochastic(current_price, low_prices, high_prices, period, stochastic_list=[]):
     # %K = (recent closing price - lowest price in period) / (highest price in period - lowest)
-    pass
+    # %D = 3 period SMA of %K
+    for index, price in enumerate(low_prices[:period]):
+        if index == 0:
+            lowest_price = float(price)
+        else:
+            if float(price) < lowest_price:
+                lowest_price = float(price)
 
-buying_thresh = 1.95
-selling_thresh = 2.5
+    for index, price in enumerate(high_prices[:period]):
+        if index == 0:
+            highest_price = float(price)
+        else:
+            if float(price) > highest_price:
+                highest_price = float(price)
+
+    percent_k = (current_price - lowest_price) / (highest_price - lowest_price) * 100
+    stochastic_list.append(percent_k)
+    sma = 0
+    for value in stochastic_list:
+        sma += value
+    sma /= period
+
+    percent_k_sma[index_num].append(percent_k)
+    if len(percent_k_sma[index_num]) > 3:
+        percent_k_sma[index_num].pop(0)
+    percent_d = 0
+    for x in percent_k_sma[index_num]:
+        percent_d += x
+    percent_d /= 3
+    print('%K:', percent_k)
+    print('%D:', percent_d)
+
+    if len(stochastic_list) == period and pd.isnull(all_share_data.iloc[share_index[index_num]]['Stochastic']):
+        all_share_data.loc[share_index[index_num], 'Stochastic'] = sma
+
+    elif not pd.isnull(all_share_data.iloc[share_index[index_num]]['Stochastic']):
+       
+        if percent_k >= 80:
+            signal = 'SELL'
+        elif percent_k - percent_d > 0 and percent_k <= 20:
+            signal = 'BUY'
+        else:
+            signal = 'HOLD'
+
+        return signal
+        
+
+
 top_of_hour_share_price = 0
-share_price = 1.
-shares = 0
-buying_power = 25000  # $ Amount in user's account
+buying_power = 1000  # $ Amount in user's account
 prev_buying_power = buying_power
-share_handler = buying_power / share_price * 0.5 # Amount of shares purchased in one interaction
-non_adjustable_min_cash_limit = 4000.
-show_closed_status = not check_market_open()
-
-# Exponential Moving average gives weight to recent price changes and captures the movement of trend quickly
-ema_twleve = 0    # 12 period
-ema_two_six = 0    # 26 period
-macd_ema = 0    # 9 period
-sma_twelve = []
-sma_two_six = []
-macd_sma = []
-
-# EMA uses data from days prior to determine new EMA
-past_ema_twelve= 0
-past_ema_two_six = 0
-past_macd_ema = 0
-
-all_share_data = pd.read_csv('ideal_formatted.csv',index_col=False)
+non_adjustable_min_cash_limit = 400.
+macd_sma = [[]]
+stochastic_sma = [[]]
+percent_k_sma = [[]]
+share_data = []
+share_index = []
 username = input('Please enter your username:')
 password = input('Please enter your password:')
-share_data = []
 
-for index, raw_share_info in all_share_data.iterrows():
-    #print(raw_share_info['Volume'])
-    if float(raw_share_info['Volume']) > 20000000:
-        share_data.append(raw_share_info['Symbol'])
-        print(raw_share_info['Symbol'])
+
 try:
     login = stonks.login(username, password, store_session=True)
 
@@ -126,92 +175,189 @@ if login:
 
 
 if check_market_open() == False:
-    print('Stock market is currently closed')
+    print('Stock market is currently closed\n')
 
 else:
-    print('Stock market is currently open')
+    print('Stock market is currently open\n')
 
-for stock in share_data:
-    past_close_prices = stonks.get_stock_historicals(stock,interval='day',span='3month',info='close_price')
-    past_low_prices = stonks.get_stock_historicals(stock,interval='day',span='3month',info='low_price')
-    past_high_prices = stonks.get_stock_historicals(stock,interval='day',span='3month',info='high_price')
-    current_price = float(stonks.get_latest_price(stock)[0])
-    
-    for index, low_price in enumerate(past_low_prices[:14]):
-        if index == 0:
-            lowest_price = float(low_price)
-        else:
-            if lowest_price > float(low_price):
-                lowest_price = float(low_price)
-    for index, high_price in enumerate(past_high_prices[:14]):
-        if index == 0:
-            highest_price = float(high_price)
-        else:
-            if highest_price > float(high_price):
-                highest_price = float(high_price)
+all_share_data = pd.read_csv('new_stuff.csv',index_col=False)
 
-    print('Lowest price:', lowest_price)
-    print('Highest price:', highest_price)
-    print('Current price:', current_price)
+# Gets volatility of a stock
+#for index, new_share_info in all_share_data.iterrows():
+#    print(new_share_info['Symbol'])
+#    calculated_returns = np.zeros(21)
+#    deviations = np.zeros(21)
+#    closing_prices = stonks.get_stock_historicals(new_share_info['Symbol'], interval='day', span='3month', info='close_price')
+#    #print(closing_prices)
+#    reversed_prices = closing_prices[::-1]
+#    #print(reversed_prices)
+
+#    for num_index, x in enumerate(closing_prices[:21]):
+#        if num_index != 0:
+#            calculated_returns[num_index] = np.log(float(x) / prev_x)
+#        else:
+#            calculated_returns[num_index] = float(x)
+#        prev_x = float(x)
+#        if calculated_returns[num_index] == 0:
+#            break
+#        #print(calculated_returns[num_index])
+#    if len(calculated_returns) == 21:
+#        mean = 0
+#        for x in calculated_returns:
+#            mean += float(x)
+#        mean /= len(calculated_returns)
+#        for num_index, x in enumerate(calculated_returns):
+#            deviations[num_index] = x - mean
+#        variance = 0
+#        for x in deviations:
+#            variance += x ** 2
+#        variance /= len(deviations) - 1
+#        volatility = math.sqrt(variance)
+#        print(volatility)
+#        all_share_data.loc[index, 'Volatility'] = volatility
+
+#print('Hello')
+#for index, new_share_info in all_share_data.iterrows():
+#    print(new_share_info['Symbol'])
+#    closing_prices = stonks.get_fundamentals(new_share_info['Symbol'], info='average_volume')
+#    all_share_data.loc[index, 'Volume'] = volume
+
+#all_share_data.to_csv('stuff.csv',index=False)
+for index, raw_share_info in all_share_data.iterrows():
+    #print(raw_share_info['Volume'])
+    if float(raw_share_info['Volatility']) > 3 and float(raw_share_info['Volume']) > 5000000:
+        share_data.append(raw_share_info['Symbol'])
+        share_index.append(index)
+        print(raw_share_info['Symbol'])
+
+print()
+shares = np.zeros(len(share_data))
+bought_price = np.zeros(len(share_data))
+past_macd_signal = np.full(len(share_data), 'None')
+past_stochastic_signal = np.full(len(share_data), 'None')
+while True:
+    if is_time_interval(second=30):
+        print('Checking for potential trades...')
+        for index_num, stock in enumerate(share_data):
+            stochastic_sma.append([])
+            macd_sma.append([])
+            stochastic_sma.append([])
+            past_close_prices = stonks.get_stock_historicals(stock,interval='day',span='3month',info='close_price')
+            past_low_prices = stonks.get_stock_historicals(stock,interval='day',span='3month',info='low_price')
+            past_high_prices = stonks.get_stock_historicals(stock,interval='day',span='3month',info='high_price')
+            current_price = float(stonks.get_latest_price(stock)[0])
+            share_handler = buying_power / current_price * 0.1 # Amount of shares purchased in one interaction
+            if share_handler < 1:
+                share_handler = 1
+            #ema_twelve = get_ema(current_price, period=12,past_close_prices=past_close_prices)
+            ema_twelve = get_ema(current_price, period=12, past_close_prices=past_close_prices)
+            ema_two_six = get_ema(current_price, period=26, past_close_prices=past_close_prices)
+            if pd.isnull(all_share_data.iloc[share_index[index_num]]['MACD']):
+                macd_signal = get_macd(ema_twelve, ema_two_six, period=9, sma_list=macd_sma[index_num])
+            else:
+                macd_signal = get_macd(ema_twelve, ema_two_six, period=9)
+            if pd.isnull(all_share_data.iloc[share_index[index_num]]['Stochastic']):
+                stochastic_signal = get_stochastic(current_price, past_low_prices, past_high_prices, period=14, stochastic_list=stochastic_sma[index_num])
+            else:
+                stochastic_signal = get_stochastic(current_price, past_low_prices, past_high_prices, period=14)
+
+            if shares[index_num] > 0:
+                print('Stock:', stock)
+                print('Bought Price:', bought_price[index_num])
+                print('MACD signal:', macd_signal)
+                print('Stochastic signal:', stochastic_signal, '\n')
+            #print(index_num)
+            #print('Current stock:', stock)
+            #print('MACD signal:', macd_signal)
+            #print('Stochastic signal:', stochastic_signal, '\n')
+            
+            if macd_signal == 'SELL' and stochastic_signal == 'SELL' and shares[index_num] != 0 and float(current_price) > bought_price[index_num]:
+                print('Selling stock for:', stock)
+                buying_power += shares[index_num] * current_price
+                shares[index_num] = 0
+                print('Previous Buying Power:', prev_buying_power)
+                print('Shares profit:', current_price - bought_price[index_num])
+                print('Current Buying Power:', buying_power, '\n')
+                prev_buying_power = buying_power
+                    
+
+            if macd_signal == 'BUY' and past_stochastic_signal[index_num] == 'BUY' and shares[index_num] == 0 and buying_power - (int(share_handler) * current_price) >= non_adjustable_min_cash_limit:
+                print('Buying stock for:', stock)
+                shares[index_num] += int(share_handler)
+                buying_power -= int(share_handler) * current_price
+                bought_price[index_num] = current_price
+                print('Number of shares:', shares[index_num])
+                print('Share price:', bought_price[index_num])
+                print('Previous Buying Power:', prev_buying_power)
+                print('Current Buying Power:', buying_power, '\n')
+                prev_buying_power = buying_power
+                   
+            #past_macd_signal[index_num] = macd_signal
+            past_stochastic_signal[index_num] = stochastic_signal
+        all_share_data.to_csv('new_stuff.csv',index=False)
 
 while check_market_open():
+        if is_time_interval(second=30):
+            print('Checking for potential trades...')
+            for index_num, stock in enumerate(share_data):
+                stochastic_sma.append([])
+                macd_sma.append([])
+                stochastic_sma.append([])
+                past_close_prices = stonks.get_stock_historicals(stock,interval='day',span='3month',info='close_price')
+                past_low_prices = stonks.get_stock_historicals(stock,interval='day',span='3month',info='low_price')
+                past_high_prices = stonks.get_stock_historicals(stock,interval='day',span='3month',info='high_price')
+                current_price = float(stonks.get_latest_price(stock)[0])
+                share_handler = buying_power / current_price * 0.1 # Amount of shares purchased in one interaction
+                if share_handler < 1:
+                    share_handler = 1
+                #ema_twelve = get_ema(current_price, period=12,past_close_prices=past_close_prices)
+                ema_twelve = get_ema(current_price, period=12, past_close_prices=past_close_prices)
+                ema_two_six = get_ema(current_price, period=26, past_close_prices=past_close_prices)
+                if pd.isnull(all_share_data.iloc[share_index[index_num]]['MACD']):
+                    macd_signal = get_macd(ema_twelve, ema_two_six, period=9, sma_list=macd_sma[index_num])
+                else:
+                    macd_signal = get_macd(ema_twelve, ema_two_six, period=9)
+                if pd.isnull(all_share_data.iloc[share_index[index_num]]['Stochastic']):
+                    stochastic_signal = get_stochastic(current_price, past_low_prices, past_high_prices, period=14, stochastic_list=stochastic_sma[index_num])
+                else:
+                    stochastic_signal = get_stochastic(current_price, past_low_prices, past_high_prices, period=14)
 
-    for stock in share_data:
-        print(stock + ':', stonks.get_stock_historicals(stock,interval='day',span='month',info='close_price'))
-        #print(raw_data.values)
-        #for stock in raw_data.values:
-            #print('Data:', stock[0])
-            #share_price.append(stonks.get_fundamentals(stock[0], info='volume'))
+                if shares[index_num] > 0:
+                    print('Stock:', stock)
+                    print('Bought Price:', bought_price[index_num])
+                    print('MACD signal:', macd_signal)
+                    print('Stochastic signal:', stochastic_signal, '\n')
+                #print(index_num)
+                #print('Current stock:', stock)
+                #print('MACD signal:', macd_signal)
+                #print('Stochastic signal:', stochastic_signal, '\n')
             
-        #print(share_price)
-        #raw_data.insert(1, 'Volume', share_price, True)
-        #raw_data.to_csv('new_formatted.csv',index=False)
+                if macd_signal == 'SELL' and stochastic_signal == 'SELL' and shares[index_num] != 0 and float(current_price) > bought_price[index_num]:
+                    print('Selling stock for:', stock)
+                    buying_power += shares[index_num] * current_price
+                    shares[index_num] = 0
+                    print('Previous Buying Power:', prev_buying_power)
+                    print('Shares profit:', current_price - bought_price[index_num])
+                    print('Current Buying Power:', buying_power, '\n')
+                    prev_buying_power = buying_power
+                    
+
+                if macd_signal == 'BUY' and past_stochastic_signal[index_num] == 'BUY' and shares[index_num] == 0 and buying_power - (int(share_handler) * current_price) >= non_adjustable_min_cash_limit:
+                    print('Buying stock for:', stock)
+                    shares[index_num] += int(share_handler)
+                    buying_power -= int(share_handler) * current_price
+                    bought_price[index_num] = current_price
+                    print('Number of shares:', shares[index_num])
+                    print('Share price:', bought_price[index_num])
+                    print('Previous Buying Power:', prev_buying_power)
+                    print('Current Buying Power:', buying_power, '\n')
+                    prev_buying_power = buying_power
+                   
+                #past_macd_signal[index_num] = macd_signal
+                past_stochastic_signal[index_num] = stochastic_signal
+            all_share_data.to_csv('new_stuff.csv',index=False)
+            #print()
         
         #my_account = stonks.profiles.load_account_profile()
         #buying_power = my_account['buying_power']
        
-        if is_time_interval(hour = 1):
-            top_of_hour_share_price = float(share_price[0])
-
-        else: 
-            if top_of_hour_share_price == 0:
-                top_of_hour_share_price = float(share_price[0]) * 1.21
-
-       
-        if is_time_interval(minute=2,second=0):
-            if past_ema_twelve != 0 and past_ema_two_six != 0 and past_macd_ema != 0:
-                ema_twelve, past_ema_twelve = get_ema(current_value=float(share_price[0]), period=12, past_ema=past_ema_twelve)
-                ema_two_six, past_ema_two_six = get_ema(current_value=float(share_price[0]), period=26, past_ema=past_ema_two_six)
-                macd_ema, past_macd_ema = get_ema(current_value=(ema_twelve - ema_two_six), period=9, past_ema=past_macd_ema)
-            else:
-                ema_twelve, sma_twelve = get_ema(current_value=float(share_price[0]), period=12, sma_list=sma_twelve)
-                ema_two_six, sma_two_six = get_ema(current_value=float(share_price[0]), period=26, sma_list=sma_two_six)
-                macd_ema, macd_sma = get_ema(current_value=(ema_twelve - ema_two_six), period=9, sma_list=macd_sma)
-
-            print('12 period Ema:', ema_twelve, ' sma\'s:', len(sma_twelve))
-            print('26 period Ema:', ema_two_six, ' sma\'s:', len(sma_two_six))
-            print('9 period Ema:', macd_ema, ' sma\'s:', len(macd_sma))
-
-        if prev_buying_power != buying_power:
-            print('Money in account: ',buying_power)
-            print('Price of stock:', share_price)
-            print('Amount of shares owned:', shares)
-            prev_buying_power = buying_power
-        
-        if past_ema_twelve != 0 and past_ema_two_six != 0 and past_macd_ema != 0:
-            macd = ema_twelve - ema_two_six
-            macd_histo = macd - macd_ema
-            print('MACD Histogram:', macd_histo)
-            
-            if macd_histo <= macd  and float(share_price[0]) <= .95 * top_of_hour_share_price:
-                while buying_power - float(share_price[0]) >= non_adjustable_min_cash_limit:
-                    shares += 1
-                    buying_power -= float(share_price[0])
-
-                bought_price = float(share_price[0])
-        
-        if shares != 0:
-            if float(share_price[0]) >= 1.05 * bought_price and past_ema_one - past_ema_two >= 0.05:
-                    buying_power += shares * float(share_price[0])
-                    shares = 0
-
